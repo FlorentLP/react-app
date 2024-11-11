60a7964fb17dddc8f3b0f6b054bee96130eb5419
florent <florent.le.pape@gmail.com>
2024-11-11 09:34:19 +0100

    learned children

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,11 @@
-import ListGroup from "./components/ListGroup.tsx";
+import Alert from "./components/Alert.tsx";
 
 function App() {
-  const items = [
-    "Get out fast of bed",
-    "Cold shower",
-    "Drink a glass of water",
-    "get dressed",
-  ];
-  const handleSelectItem = (item: string) => {
-    console.log(item);
-  };
   return (
     <div>
-      <ListGroup
-        items={items}
-        heading="Habits"
-        onSelectItem={handleSelectItem}
-      ></ListGroup>
+      <Alert>
+        <h1>HELP HELP I WOULD LIKE SOME HELP</h1>
+      </Alert>
     </div>
   );
 }
--- src/components/Alert.tsx
+++ src/components/Alert.tsx
@@ -1,4 +1,10 @@
-const Alert = () => {
-  return <div>Alert</div>;
+import { ReactNode } from "react";
+
+interface Props {
+  children: ReactNode;
+}
+
+const Alert = ({ children }: Props) => {
+  return <div className="alert alert-danger">{children}</div>;
 };
 export default Alert;
